288a6731d0e73b7af39eb0cec0eafffc832935e8
Cindy Vo <yendai.vo@hotmail.com>
2018-05-19 22:21:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function theBeatlesPlay(musicians, instruments){
   
   var musicianWithInstrument=[];
   var i;
-  for (i=0; i<musicians.length; ) {
-    musicianWithInstrument[i]= musicians[i]+ " plays "+ instruments[i];i++
+  for (i=0; i<musicians.length; i++) {
+    musicianWithInstrument[i]= musicians[i]+ " plays "+ instruments[i];
   }
   return musicianWithInstrument;
 }
