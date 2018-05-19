a75f436aaa2e5020c23339d94fc8f1016b2f433b
Cindy Vo <yendai.vo@hotmail.com>
2018-05-19 22:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function theBeatlesPlay(musicians, instruments){
-  var musicianWithInstrument;
+  var musicianWithInstrument = [];
   var i;
   for (i=0; i<musicians.length; i++) {
     musicianWithInstrument[i] = `${musicians[i]} plays ${instruments[i]}`
