dae17aa56176f04a1dda3cb53df2279174d9ae87
Cindy Vo <yendai.vo@hotmail.com>
2018-05-19 22:25:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function theBeatlesPlay(musicians, instruments){
   var musicians = [
     "John Lennon",
-    "Paul Mccarthy"
+    "Paul Mccarthy"1
   ]
   
   var instruments = [
     "Guitar",
-    "Drums"
+    "Drums"1
   ]
   
   var musicianWithInstrument=[
     "",0
-    ""
+    ""1
   ];
   var i;
   for (i=0; i<musicians.length; i++) {
