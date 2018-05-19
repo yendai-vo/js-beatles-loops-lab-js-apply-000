bef483db4357508ba7f07100abab2b189b203942
Cindy Vo <yendai.vo@hotmail.com>
2018-05-19 22:18:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
 function theBeatlesPlay(musicians, instruments){
-  var musicianWithInstrument=[];
   var i;
   for (i=0; i<musicians.length; i++) {
+    var musicianWithInstrument=[];
     musicianWithInstrument[i]= musicians[i]+ " plays "+ instruments[i];
   }
   return musicianWithInstrument;
 }
-
-
-for(var i=0; i < musicians.length; i++){
-  
-}
-
-
-theBeatlesPlay(
-  [
-    "John Lennon",  0
-    "Paul Mccarthy" 1
-  ],
-  [
-    "Guitar", 0
-    "Drums"   1
-  ]
-)
-
-"John lennon plays guitar"
-"Paul mccarythy plays drums"
